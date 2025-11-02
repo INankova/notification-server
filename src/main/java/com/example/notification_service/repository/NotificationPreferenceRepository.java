9bfe7d44cd79a3a8e5b7123f197daa7a9f5c8711
INankova <ivana.nankova@gmial.com>
2025-11-02 12:37:46 +0200

    chore: add method to find enabled notification preferences by type

--- src/main/java/com/example/notification_service/repository/NotificationPreferenceRepository.java
+++ src/main/java/com/example/notification_service/repository/NotificationPreferenceRepository.java
@@ -1,9 +1,11 @@
 package com.example.notification_service.repository;
 
 import com.example.notification_service.model.NotificationPreference;
+import com.example.notification_service.model.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +13,6 @@ import java.util.UUID;
 public interface NotificationPreferenceRepository extends JpaRepository<NotificationPreference, UUID> {
 
     Optional<NotificationPreference> findByUserId(UUID uuid);
+
+    List<NotificationPreference> findAllByEnabledTrueAndType(NotificationType type);
 }
